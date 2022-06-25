6546db6d11921bab7fd0f96d98f3d22b4d6f5732
Phelipe97 <phelipe.ph25@gmail.com>
2022-06-25 10:03:33 -0300

    adicionando 2 consoles .log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema gerenciamento de jogos");
 console.log("apicação rodando no banco de dados");
+//log de texte
+console.log("commit feito por mim msm");
+console.log("enviando par o git hub")
